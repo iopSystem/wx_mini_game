f89b65de3a00e4594aef80cc93ab8a4e51ba2c9d
sheep <1264768027@qq.com>
2024-06-28 14:03:19 +0800

    测试

--- temp/declarations/jsb.d.ts
+++ temp/declarations/jsb.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="F:\CocosCreateEdit\cocos\editors\Creator\3.8.3\resources\resources\3d\engine\@types\jsb.d.ts"/>
+/// <reference path="D:\ProgramData\cocos\editors\Creator\3.8.3\resources\resources\3d\engine\@types\jsb.d.ts"/>
---
+++ temp/programming/packer-driver/targets/preview/chunks/ad/ada42237649f5729b692bdec9362eb96172033d7.js
@@ -0,0 +1,39 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, _dec, _class, _crd, ccclass, property, aaadd;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "962c5l/eC9Pjoeb7qANSjqn", "aaadd", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Node']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("aaadd", aaadd = (_dec = ccclass('aaadd'), _dec(_class = class aaadd extends Component {
+        start() {}
+
+        update(deltaTime) {}
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=ada42237649f5729b692bdec9362eb96172033d7.js.map
